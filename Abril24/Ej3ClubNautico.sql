dfa2c3217b8c4338d0a795acba379382c9047c7f
José Antonio Fdez-Montes García <123330906+Ufb77@users.noreply.github.com>
2024-04-24 15:06:27 +0200

    Add files via upload

---
+++ Abril24/Ej3ClubNautico.sql
@@ -0,0 +1,52 @@
+--Ejercicio 3
+
+CREATE TABLE IF NOT EXISTS socios(
+
+	id SERIAL PRIMARY KEY,
+	nombre varchar(100),
+	direccion varchar(200)
+
+	
+);
+
+CREATE TABLE IF NOT EXISTS patrones(
+
+	id SERIAL PRIMARY KEY,
+	dni varchar(50) UNIQUE,
+	nombre varchar(100),
+	direccion varchar(200)
+
+	
+);
+
+CREATE TABLE IF NOT EXISTS barcos(
+
+	id SERIAL PRIMARY KEY,
+	matricula varchar(100) UNIQUE,
+	direccion varchar(200),
+	n_amarre int,
+	cuota numeric(5, 2),
+	socio_id int,
+	CONSTRAINT fk_socio_id FOREIGN KEY (socio_id) REFERENCES socios(id) ON DELETE CASCADE ON UPDATE CASCADE
+	
+
+	
+);
+
+
+CREATE TABLE IF NOT EXISTS salidas(
+
+	id SERIAL PRIMARY KEY,
+	destino varchar(100),
+	f_salida date,
+	h_salida time,
+	barco_id int,
+	patron_id int,
+	CONSTRAINT fk_barco_id FOREIGN KEY (barco_id) REFERENCES barcos(id) ON DELETE CASCADE ON UPDATE CASCADE,
+	CONSTRAINT fk_patron_id FOREIGN KEY (patron_id) REFERENCES patrones(id) ON DELETE CASCADE ON UPDATE CASCADE
+	
+
+	
+	
+);
+
---
+++ Abril24/Ej4Tienda.sql
@@ -0,0 +1,103 @@
+--Ejercicio 4
+
+CREATE TABLE IF NOT EXISTS socios(
+
+	id SERIAL PRIMARY KEY,
+	nombre varchar(100),
+	apellidos varchar(100),
+	dni varchar(100) UNIQUE,
+	telefono varchar(20),
+	socio_avalador_id int,
+	poblacion_id int,
+	CONSTRAINT fk_socio_id FOREIGN KEY (socio_avalador_id) REFERENCES socios(id) ON DELETE CASCADE ON UPDATE CASCADE,
+	CONSTRAINT fk_poblacion_id FOREIGN KEY (poblacion_id) REFERENCES poblaciones(id) ON DELETE CASCADE ON UPDATE CASCADE
+
+
+)
+
+CREATE TABLE IF NOT EXISTS poblaciones(
+
+	id SERIAL PRIMARY KEY,
+	habitantes int,
+	nombre varchar(100),
+	provincia_id int,
+	CONSTRAINT fk_provincia_id FOREIGN KEY (provincia_id) REFERENCES provincia(id) ON DELETE CASCADE ON UPDATE CASCADE
+)
+
+CREATE TABLE IF NOT EXISTS provincia(
+
+	id SERIAL PRIMARY KEY,
+	nombre varchar(100),
+	p_extension numeric(10, 2),
+	almacen_id int,
+	CONSTRAINT fk_almacen_id FOREIGN KEY (almacen_id) REFERENCES almacenes(id) ON DELETE CASCADE ON UPDATE CASCADE
+)
+
+CREATE TABLE IF NOT EXISTS almacenes(
+
+	id SERIAL PRIMARY KEY,
+	nombre varchar(100),
+	f_apertura date,
+	direccion varchar(100),
+	provincia_id int
+	
+	
+	
+)
+
+CREATE TABLE IF NOT EXISTS pedidos(
+
+	id SERIAL PRIMARY KEY,
+	forma_envio varchar(100),
+	forma_pago varchar(100),
+	socio_id int,
+	CONSTRAINT fk_socio_id FOREIGN KEY (socio_id) REFERENCES socios(id) ON DELETE CASCADE ON UPDATE CASCADE
+	
+	
+
+)
+
+CREATE TABLE IF NOT EXISTS detalles_pedidos(
+
+	
+	cantidad int,
+	consecutivo int,
+	isbn varchar(100),
+	pedido_id int,
+	CONSTRAINT fk_pedido_id FOREIGN KEY (pedido_id) REFERENCES pedidos(id) ON DELETE CASCADE ON UPDATE CASCADE,
+	CONSTRAINT fk_isbn FOREIGN KEY (isbn) REFERENCES libros(isbn) ON DELETE CASCADE ON UPDATE CASCADE,
+	CONSTRAINT pk_detalles_pedido PRIMARY KEY (consecutivo, pedido_id)
+	
+)
+
+CREATE TABLE IF NOT EXISTS libros(
+
+	isbn varchar(100) PRIMARY KEY,
+	titulo varchar(100),
+	autor varchar(200),
+	editorial varchar(100),
+	coleccion_id int,
+	CONSTRAINT fk_coleccion FOREIGN KEY (coleccion_id) REFERENCES colecciones(id) ON DELETE CASCADE ON UPDATE CASCADE
+	
+
+)
+
+CREATE TABLE IF NOT EXISTS stock_libros_almacenes(
+
+	isbn varchar(100),
+	almacen_id int,
+	cantidad int,
+	CONSTRAINT fk_isbn FOREIGN KEY (isbn) REFERENCES libros(isbn) ON DELETE CASCADE ON UPDATE CASCADE,
+	CONSTRAINT fk_almacen_id FOREIGN KEY (almacen_id) REFERENCES almacenes(id) ON DELETE CASCADE ON UPDATE CASCADE,
+	CONSTRAINT pk_stock_libros_almacenes PRIMARY KEY (isbn, almacen_id)
+	
+
+)
+
+CREATE TABLE IF NOT EXISTS colecciones(
+
+	id SERIAL PRIMARY KEY,
+	volumen int
+	
+
+);
